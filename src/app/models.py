fc1ca06fb03807a346144e4f46a2e74dea20a592
Alex0M <alex.sudom@gmail.com>
2018-03-30 17:08:25 +0300

    add Flask-SQLAlchemy

---
+++ src/app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
--- src/config.py
+++ src/config.py
@@ -3,3 +3,5 @@ SECRET_KEY = 'pNRMSb1MVMz3wmQ7ypnY1TWim4CFQVsc'
 
 MONGO_DBNAME = 'test'
 MONGO_URI = 'mongodb://mongodb:27017/test'
+
+SQLALCHEMY_DATABASE_URI = "mysql://fwallet:fwallet@mysqldb/fwallet"
